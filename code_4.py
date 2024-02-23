fcfd5fc860a1984bc0fa6e68ac14a6e7da2675e6
vnieto2 <156244045+vnieto2@users.noreply.github.com>
2024-02-23 15:34:55 -0700

    update_code4.py

--- code_4.py
+++ code_4.py
@@ -2,11 +2,21 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
+df = pd.read_csv(big_mac_file)
+
+# print (df)
+# print (df['dollar_price'])
+
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    query = f'(iso_a3 == {country_code})'
+    mxn_df = df.query(query)
+    print(round(mxn_df['dollar_price'].mean(),2))
+
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    query = f'(iso_a3 == {country_code} and ({year} > 2019-01-01 and {year} < 2019-12-31))'
+    mxn_df = df.query(query)
+    print(round(mxn_df['dollar_price'].mean(),2))
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
@@ -15,4 +25,4 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    get_big_mac_price_by_year(country_code='mex') 
