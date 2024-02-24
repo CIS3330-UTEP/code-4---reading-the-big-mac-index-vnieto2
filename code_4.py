4563a5a96dce2fa5d7e216327cb58e47b1b646d9
vnieto2 <156244045+vnieto2@users.noreply.github.com>
2024-02-23 22:49:12 -0700

    update_code_4.py

--- code_4.py
+++ code_4.py
@@ -4,30 +4,33 @@ big_mac_file = './big-mac-full-index.csv'
 
 df = pd.read_csv(big_mac_file)
 
-# print (df)
-# print (df['iso_a3'])
 
-
-
-def get_big_mac_price_by_year(year, country_code):
-    country_code = 'jpn'
-    year = 2016
-    query = f'(iso_a3 == {country_code} and date == {year})'
+def get_big_mac_price_by_year(year,country_code):
+    query = f'(iso_a3 == {country_code} and date = {year})'
     jpn_df = df.query(query)
-    print(round(jpn_df['dollar_price'].mean(),2)) 
+    return round(jpn_df['dollar_price'].mean(),2)
 
 def get_big_mac_price_by_country(country_code):
-    pass
-    #  query = f'(iso_a3 == {country_code})'
-    # mxn_df = df.query(query)
-    # print(round(mxn_df['dollar_price'].mean(),2))
+    query = f'(iso_a3 == {country_code})'
+    jpn_df = df.query(query)
+    return round(jpn_df['dollar_price'].mean(),2)
 
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    query = f'(date == {year})'
+    cheap_df = df.query(query)
+    return cheap_df['dollar_price'].min()
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    query = f'(date == {year})'
+    rich_df = df.query(query)
+    return rich_df['dollar_price'].max()
 
 if __name__ == "__main__":
-    get_big_mac_price_by_year(year= 2016,country_code='jpn')
+    print(get_big_mac_price_by_year(2016,'jpn'))
+
+    print(get_big_mac_price_by_country('jpn'))
+
+    print(get_the_cheapest_big_mac_price_by_year(2016))
+
+    print(get_the_most_expensive_big_mac_price_by_year(2016))
